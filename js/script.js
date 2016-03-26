7af452da14d6212238cf17aa28524bee5168a34e
GitDevv <solutionrise@ukr.net>
2016-03-26 04:29:09 +0200

    tovat cart commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
+	function updateSpinner(obj)
+	{
+		var contentObj = document.getElementById("content_num");
+		var value = parseInt(contentObj.value);
+		
+		var valCost = parseInt($('.new_cost input').val());
+		var Cost = parseInt($('.cost input').val());
+		var discount = parseInt($('.discount input').val());
+		var subCost;
+		
+		subCost = Cost - discount;
+		if(obj.id == "down") {
+			value--;
+			valCost = subCost * value;
+			if(value < 1){
+				value = Math.max(2, input.value - 1);
+			}
+		} else {
+			value++;
+			valCost = subCost * value;
+//			if(value > 10){
+//				value = Math.max(10, input.value - 1);
+//			}			
+		}		
+		
+		$('.new_cost input').val(valCost);
+		contentObj.value = value;
+		
+		$('.new_cost span').html($('.new_cost input').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));			
+		
+	}	 
+
 $(document).ready(function(){
 	 
+ 
  
 	$('.top-slider').slick({
         slidesToShow: 1,
