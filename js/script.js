515370dedde8062a239ea29690b2e452b9b82502
GitDevv <solutionrise@ukr.net>
2016-03-28 19:13:46 +0300

    last commit

--- js/script.js
+++ js/script.js
@@ -1,125 +1,103 @@
-	function updateSpinner(obj)
-	{
-		var contentObj = document.getElementById("content_num");
-		var value = parseInt(contentObj.value);
-		
-		var valCost = parseInt($('.new_cost input').val());
-		var Cost = parseInt($('.cost input').val());
-		var discount = parseInt($('.discount input').val());
-		var subCost;
-		
-		subCost = Cost - discount;
-		if(obj.id == "down") {
-			value--;
-			valCost = subCost * value;
-			if(value < 1){
-				value = Math.max(2, input.value - 1);
-			}
-		} else {
-			value++;
-			valCost = subCost * value;
-//			if(value > 10){
-//				value = Math.max(10, input.value - 1);
-//			}			
-		}		
-		
-		$('.new_cost input').val(valCost);
-		contentObj.value = value;
-		
-		$('.new_cost span').html($('.new_cost input').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));			
-		
-	}	 
+function updateSpinner(obj)
+{
+	var contentObj = document.getElementById("content_num");
+	var value = parseInt(contentObj.value);
 
+	var valCost = parseInt($('.new_cost input').val());
+	var Cost = parseInt($('.prod_inf_list .cost_li input').val());
+	var discount = parseInt($('.discount input').val());
+	var subCost = 0;
+
+	subCost = Cost - discount;
+	if(obj.id == "down") {
+		value--;
+		valCost = subCost * value;
+		if(value < 1){
+			value = Math.max(1, input.value - 1);
+		}
+	} else {
+		value++;
+		valCost = subCost * value;			
+	}		
+
+	$('.new_cost input').val(valCost);
+	contentObj.value = value;
+
+	$('.new_cost span').html($('.new_cost input').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));			
+
+}	
+
+
+
+//function toCartProduct(){
+//		var $this = $(this); //знаходжу кнопку на котру натискаю
+//		var valueCost =  parseInt($(this).parent().find('.new_costt').children('input').val());
+//		var fullcost = parseInt($('.in_full_cost').val());
+//		var fullnumber = parseInt($('.in_full_num').val());
+//
+//		$('.in_full_cost').val(fullcost + valueCost);
+////		console.log($('.in_full_cost').val());
+//		$('.in_full_num').val(fullnumber + 1);
+////		console.log($('.in_full_num').val());
+//		
+//		$('.cart .fullcost').html($('.in_full_cost').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));	
+//		$('.cart .fullnumber').html($('.in_full_num').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));		
+//}
+
+
+/*В корзину button*/
 $(document).ready(function(){
-	 
- 
- 
-	$('.top-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow:'.wraper-top-slider .cont-slider .prev',
-        nextArrow:'.wraper-top-slider .cont-slider .next',
-        autoplay:false,
-		draggable:false,
-		fade:true,
-		dots:true,
-		speed: 1000,
-     });	
 	
-        $(window).scroll(function(){
-           if ($(this).scrollTop() > 60  ){
-                 $('.middle-container').addClass('active');
-            } else {
-				$('.middle-container').removeClass('active');
-			} 
-        });//scroll	
-	
-	
-	 $('input[name="phone"]').mask("+7(999) 999 - 99 - 99");
-	
-	$('.callback').click(function(){
-		$('.modal_callback , .modal_callback .wp_callback').fadeIn();
-		$(' .modal_callback .wp_sp, .modal_callback .wp_write').fadeOut();
-		$('body').css('overflow-y','hidden');
-		
-	});	
-	
-	$('.write_us').click(function(){
-		$('.modal_callback , .modal_callback .wp_write').fadeIn();
-		$(' .modal_callback .wp_sp, .modal_callback .wp_callback').fadeOut();
-		$('body').css('overflow-y','hidden');
-	});	
+	$('.to_cartt').click(function(){
+		var $this = $(this); //знаходжу кнопку на котру натискаю
+		var valueCost =  parseInt($(this).parent().find('.new_costt').children('input').val());
+		var fullcost = parseInt($('.in_full_cost').val());
+		var fullnumber = parseInt($('.in_full_num').val());
 
-	$('.modal_callback .close, .modal_callback .closed').click(function(){
-		$(' .modal_callback').fadeOut();
-		$("#callback").trigger("reset");
-		$("#write").trigger("reset");
-		$('body').css('overflow-y','auto');
-	});		
-	
-	
-    $("#callback").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "callback.php",
-			data: $("#callback").serialize()
-		}).done(function() {
-            $('.modal_callback .wp_callback').fadeOut();
-			$(' .modal_callback .wp_sp').fadeIn();
-            $("#callback").trigger("reset");
-			 
-		});
-		return false;
+		$('.in_full_cost').val(fullcost + valueCost);
+//		console.log($('.in_full_cost').val());
+		$('.in_full_num').val(fullnumber + 1);
+//		console.log($('.in_full_num').val());
+		
+		$('.cart .fullcost').html($('.in_full_cost').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));	
+		$('.cart .fullnumber').html($('.in_full_num').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));				
 	}); 
 	
-    $("#write").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "write.php",
-			data: $("#write").serialize()
-		}).done(function() {
-            $('.modal_callback .wp_write').fadeOut();
-			$(' .modal_callback .wp_sp').fadeIn();
-            $("#write").trigger("reset");
-			 
-		});
-		return false;
+	$('.prod_inf_list .to_cart').click(function(){
+		var fullcost = parseInt($('.in_full_cost').val());
+		var fullnumber = parseInt($('.in_full_num').val());
+		
+		var contentObj = document.getElementById("content_num");
+		var value = parseInt(contentObj.value);		
+		var valCost = parseInt($('.new_cost input').val());
+
+		$('.in_full_cost').val(fullcost + valCost);
+//		console.log($('.in_full_cost').val());
+		$('.in_full_num').val(fullnumber + value);
+//		console.log($('.in_full_num').val());
+		
+		$('.cart .fullcost').html($('.in_full_cost').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));	
+		$('.cart .fullnumber').html($('.in_full_num').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));				
 	}); 	
-	
-	
-	/*****************/
-	$('.button_menu_inner').click(function(){
-		if(!$('.items_buy').hasClass('active')){
-			$('.items_buy, .button_menu_inner').addClass('active');
-			$('.shadow').fadeIn();
-		}else{
-			$('.items_buy, .button_menu_inner').removeClass('active');
-			$('.shadow').fadeOut();
-		}
-	});		
-	$('.shadow').click(function(){
-		$('.items_buy, .button_menu_inner').removeClass('active');
-		$('.shadow').fadeOut();
-	});		
+	 
+ 
 });
+/*В корзину button*/	
+
+$(document).ready(function(){
+	$('.carousel').slick({
+		dots:true,  
+		slidesToShow: 1,
+		slidesToScroll: 1,     
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',        
+		prevArrow:'.carousel_wrapper .controllers .prev',
+		nextArrow:'.carousel_wrapper .controllers .next'
+	}); 
 	
+ 	$('#tab-product').easytabs();
+	
+	
+});
